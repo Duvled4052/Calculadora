4485c682934b6bc86dca4ff5d3d7b93c7a09397c
Duvled4052 <153964126+Duvled4052@users.noreply.github.com>
2024-02-07 17:57:28 -0300

    Botões de multiplicar e dividir

--- Calculadora/calc2.js
+++ Calculadora/calc2.js
@@ -1,19 +1,31 @@
 const display = document.getElementById('display')
+const display2 = document.getElementById('display2')
 
 function show(aa){
-display.value += aa;
+    display.value += aa;
+    display2.value += aa;
 }
-
-function fff(){
+function showtimes(){
+        display.value += "×";
+        display2.value += '*';
+    }
+function showdivide(){
+        display.value += '÷';
+        display2.value += '/';
+    }
+function cleardisplay(){
     display.value = "";
+    display2.value = "";
 }
 
 Error = 'Error'
 function equals(aa){
     try{
-display.value = eval(display.value)
+display2.value = eval(display2.value);
+display.value = display2.value;
     }
     catch(error){
-        display.value = "Error";
+        display2.value = 'Error';
+        display.value = display2.value;
     }
 }
